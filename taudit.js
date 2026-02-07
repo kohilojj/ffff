9671ef0147b8a2d2f9a9c2eab80d7688b777be5f
kohilojj <natsaerkki@gmail.com>
2026-02-07 14:56:53 +0200

    Update taudit.js

--- taudit.js
+++ taudit.js
@@ -1,61 +1,34 @@
 // ==========================================
-// 🏥 SAIRAALA 300 - TAUTITIETOKANTA
+// 🏥 SAIRAALA 300 - TAUTILISTA
 // ==========================================
 
-// --- KATEGORIA: SYDÄN JA VERENKIERTO ---
-const sydanTaudit = [
-    { q: "Rintaa puristaa ja hiki valuu, kipu säteilee vasempaan käteen.", lab: "Troponiini koholla", img: "EKG: ST-tason nousu", opt: ["Sydäninfarkti", "Paniikkikohtaus", "Närästys"], ans: "Sydäninfarkti", info: "Infarkti vaatii välitöntä hoitoa!" },
-    { q: "Hengittäminen sattuu pistävästi toiselle puolelle rintaa.", lab: "D-dimeeri korkea", img: "Varjoainekuva: Tukos keuhkoissa", opt: ["Keuhkoveritulppa", "Lihaskipu", "Keuhkokuume"], ans: "Keuhkoveritulppa", info: "Vaatii nopean liuotus- tai estohoidon." },
-    { q: "Jalkani on turvonnut, punainen ja todella kipeä pohkeesta.", lab: "CRP 20", img: "UÄ: Laskimotukos havaittu", opt: ["Laskimotukos", "Ruusu", "Lihasvamma"], ans: "Laskimotukos", info: "Tukos voi lähteä liikkeelle keuhkoihin." },
-    { q: "Sydän hakkaa miten sattuu ja olo on hutera.", lab: "Verenpaine 130/80", img: "EKG: Eteisvärinä (ei P-aaltoja)", opt: ["Eteisvärinä", "Sydäninfarkti", "Paniikki"], ans: "Eteisvärinä", info: "Flimmeri eli eteisvärinä nostaa tukosriskiä." }
-];
-
-// --- KATEGORIA: VATSA JA SISÄELIMET ---
-const vatsaTaudit = [
-    { q: "Vatsaan sattuu oikealle alas, hyppiminen tekee kipeää.", lab: "CRP 95, leukosyytit koholla", img: "UÄ: Turvonnut umpisuoli", opt: ["Umpisuolentulehdus", "Vatsatauti", "Munuaiskivi"], ans: "Umpisuolentulehdus", info: "Appendisiitti on yleinen leikkausaihe." },
-    { q: "Söin ulkomailla ja nyt ripuli on veristä ja vatsa kramppaa.", lab: "F-Bak-seulonta positiivinen", img: "Suoliäänet vilkkaat", opt: ["Salmonella", "Nestehukka", "Keliakia"], ans: "Salmonella", info: "Ruokamyrkytys vaatii usein nesteytystä." },
-    { q: "Vatsa on kova kuin kivi ja pallo-mainen, ei toimi.", lab: "Elektrolyytit pielessä", img: "Röntgen: Suoli mutkalla", opt: ["Suolitukos", "Ummetus", "Lihavuus"], ans: "Suolitukos", info: "Ileus vaatii usein kirurgin arviota." },
-    { q: "Kova vatsakipu, joka säteilee vyömäisesti selkään.", lab: "Amylaasi pilvissä", img: "CT: Turvonnut haima", opt: ["Haimatulehdus", "Mahahaava", "Närästys"], ans: "Haimatulehdus", info: "Alkoholi tai sappikivet ovat yleisimmät syyt." }
-];
-
-// --- KATEGORIA: PÄÄ JA HERMOSTO ---
-const paaTaudit = [
-    { q: "Päässäni tuntuu pahinta kipua koskaan, kuin salama.", lab: "Verenpaine 195/110", img: "CT: Verta aivoissa", opt: ["SAV-vuoto", "Migreeni", "Jännityspäänsärky"], ans: "SAV-vuoto", info: "Lukinkalvonalainen vuoto on hengenvaarallinen." },
-    { q: "En pysty liikuttamaan vasenta kättäni ja puhe sammaltaa.", lab: "Sokeri 5.5 (normaali)", img: "Aivoissa tukos havaittu", opt: ["Aivoinfarkti", "Alkoholi", "Migreeni"], ans: "Aivoinfarkti", info: "Aika on aivoja - liuotushoito heti!" },
-    { q: "Naamani on toiselta puolelta valunut, en saa silmää kiinni.", lab: "Normaali", img: "CT: Puhdas", opt: ["Kasvohalvaus", "Aivoinfarkti", "Migreeni"], ans: "Kasvohalvaus", info: "Bellin pareesi on usein hermotulehdus." }
-];
-
-// --- KATEGORIA: TAPATURMAT JA LUUT ---
-const tapaturmat = [
-    { q: "Kaaduin liukkaalla, ranteeni on aivan mutkalla.", lab: "Ei tarvita", img: "Röntgen: Luu poikki", opt: ["Rannemurtuma", "Nyrjähdys", "Kramppi"], ans: "Rannemurtuma", info: "Murtuma vaatii kipsauksen tai leikkauksen." },
-    { q: "Polvi vääntyi sählyssä, kuului 'poks' ja se turposi.", lab: "Ei tarvita", img: "MRI: Eturistiside poikki", opt: ["ACL-vamma", "Kierukkavamma", "Kihti"], ans: "ACL-vamma", info: "Eturistiside repeää usein vääntövammoissa." }
-];
-
-// --- KATEGORIA: LISÄÄ TÄHÄN UUSIA LOHKOJA ---
-const muutTaudit = [
-    { q: "Pissatessa polttaa ja virtsa on sameaa.", lab: "Virtsassa bakteereja", img: "Ei muutoksia", opt: ["Virtsatulehdus", "Munuaiskivi", "Sukupuolitauti"], ans: "Virtsatulehdus", info: "Hoidetaan yleensä antibiootilla." },
-    { q: "Silmäni rähmii ja on aivan punainen.", lab: "CRP normaali", img: "Sidekalvon punoitus", opt: ["Silmätulehdus", "Kaihi", "Glaukooma"], ans: "Silmätulehdus", info: "Erittäin tarttuva vaiva." }
+const raakaData = [
+    // KOPIOI TÄSTÄ ALASPÄIN UUSIA RIVEJÄ:
+    { q: "Rintaa puristaa ja hiki valuu.", l: "Troponiini koholla", i: "EKG: ST-nousu", o: ["Sydäninfarkti", "Närästys", "Paniikki"], a: "Sydäninfarkti", info: "Hätätilanne!" },
+    { q: "Vatsa oikealta alhaalta kipeä.", l: "CRP 90", i: "UÄ: Umpisuoli turvassa", o: ["Umpisuoli", "Vatsatauti", "Kramppi"], a: "Umpisuoli", info: "Leikataan usein." },
+    { q: "Pissatessa polttaa kovasti.", l: "Virtsa: Bakteereja", i: "Ei löydöksiä", o: ["Virtsatulehdus", "Kivi", "Sieni"], a: "Virtsatulehdus", info: "Antibiootti auttaa." },
+    { q: "Silmä on punainen ja rähmii.", l: "Normaali", i: "Punoitusta", o: ["Silmätulehdus", "Kaihi", "Sokeus"], a: "Silmätulehdus", info: "Tarttuu herkästi." },
+    { q: "Kaaduin ja ranne on mutkalla.", l: "Ei ole", i: "Röntgen: Poikki", o: ["Murtuma", "Nyrjähdys", "Mustelma"], a: "Murtuma", info: "Kipsi 5 viikkoa." },
+    { q: "Hengitys vinkuu ja ahdistaa.", l: "Happi 90%", i: "Vinkunaa", o: ["Astma", "Flunssa", "Keuhkokuume"], a: "Astma", info: "Avaava lääke auttaa." },
+    { q: "Päässä pahin kipu ikinä.", l: "Paine 190", i: "CT: Verta", o: ["Aivoverenvuoto", "Migreeni", "Krapula"], a: "Aivoverenvuoto", info: "Heti sairaalaan!" },
+    { q: "Jalka on punainen ja kuuma.", l: "CRP 120", i: "Iho punainen", o: ["Ruusu", "Tukos", "Lihaskipu"], a: "Ruusu", info: "Antibioottia suoneen." },
+    { q: "Huimaa, maailma pyörii.", l: "Normaali", i: "Korvakivet", o: ["Asentohuimaus", "Humala", "Väsymys"], a: "Asentohuimaus", info: "Jumpalla ohi." },
+    { q: "Söin kanaa ja nyt ripuli lentää.", l: "F-Bak +", i: "Suoli vilkas", o: ["Salmonella", "Keliakia", "Nälkä"], a: "Salmonella", info: "Muista juoda vettä." }
+    // VOIT LISÄTÄ TÄHÄN SATOJA RIVEJÄ SAMALLA TAVALLA
 ];
 
 // ==========================================
-// 🛠️ AUTOMAATTINEN YHDISTÄMINEN (Älä koske)
+// ⚙️ AUTOMAATIO (Älä koske näihin)
 // ==========================================
 
-const allDiseases = [
-    ...sydanTaudit,
-    ...vatsaTaudit,
-    ...paaTaudit,
-    ...tapaturmat,
-    ...muutTaudit
-];
+// Muutetaan lyhenteet pelin ymmärtämään muotoon automaattisesti
+const allDiseases = raakaData.map(d => ({
+    q: d.q, lab: d.l, img: d.i, opt: d.o, ans: d.a, info: d.info
+}));
 
-// 👤 POTILASPROFIILIT
 const patientProfiles = [
-    { age: 5, sex: "Poika", job: "Päiväkotilainen" },
-    { age: 24, sex: "Nainen", job: "Opiskelija" },
-    { age: 42, sex: "Mies", job: "Rakennusmies" },
-    { age: 58, sex: "Mies", job: "Rekkakuski" },
-    { age: 82, sex: "Nainen", job: "Eläkeläinen" },
-    { age: 19, sex: "Mies", job: "Varusmies" },
-    { age: 35, sex: "Nainen", job: "Arkkitehti" }
+    { age: 25, sex: "Nainen", job: "Koodari" },
+    { age: 60, sex: "Mies", job: "Eläkeläinen" },
+    { age: 10, sex: "Poika", job: "Koululainen" },
+    { age: 40, sex: "Nainen", job: "Lääkäri" }
 ];
